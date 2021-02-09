5ae9d819315355b4ffc8a67b19d754a57b467bd5
Vito Gentile <gentilevito1076@gmail.com>
2021-02-09 12:28:28 +0100

    Start: v-model su input

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// https://api.themoviedb.org/3/search/movie?api_key=1eab81ae7f08840126e340e60ce049eb
+
+/*Milestone 1:
+Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
+1.	Titolo
+2.	Titolo Originale
+3.	Lingua
+4.	Voto*/
+
+new Vue({
+  el: '#app',
+  data: {
+    textSearched: ''
+
+  }
+
+
+});
+Vue.config.devtools = true;
