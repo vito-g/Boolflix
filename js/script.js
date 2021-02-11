db3f89a613abb8dd91240a5018b3126cb991dc3f
Vito Gentile <gentilevito1076@gmail.com>
2021-02-11 15:43:39 +0100

    Traccia Milestone 4 aggiunta

--- js/script.js
+++ js/script.js
@@ -24,6 +24,12 @@ https://image.tmdb.org/t/p/w342/wwemzKWzjKYJFfCeiB57q3r4Bcm.png
 (https://image.tmdb.org/t/p/w185/)
 (element.poster_path)
 -------------------------------------------------------------------------------------
+Milestone 4:
+Trasformiamo quello che abbiamo fatto fino ad ora in una vera e propria webapp, creando un layout completo simil-Netflix:
+●	Un header che contiene logo e search bar
+●	Dopo aver ricercato qualcosa nella searchbar, i risultati appaiono sotto forma di “card” in cui lo sfondo è rappresentato dall’immagine di copertina (consiglio la poster_path con w342)
+●	Andando con il mouse sopra una card (on hover), appaiono le informazioni aggiuntive già prese nei punti precedenti più la overview
+
 -------------------------------------------------------------------------------------
 */
 
